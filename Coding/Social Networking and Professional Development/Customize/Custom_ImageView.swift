0640f1287023a5ac734cf329e921cddd16098b39
perceptionfoundry <perception.khi@gmail.com>
2018-06-29 13:18:41 +0500

    color change

--- Coding/Social Networking and Professional Development/Customize/Custom_ImageView.swift
+++ Coding/Social Networking and Professional Development/Customize/Custom_ImageView.swift
@@ -10,12 +10,60 @@ import UIKit
 
 @IBDesignable class Custom_ImageView: UIImageView {
 
-    @IBInspectable var C_Radius : CGFloat = 0 {
+    @IBInspectable
+    var C_Radius : CGFloat = 0 {
         didSet{
             layer.cornerRadius = C_Radius
             layer.masksToBounds = true
         }
         
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
     }
 
 }
